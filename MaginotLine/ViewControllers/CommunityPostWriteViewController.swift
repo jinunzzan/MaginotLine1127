361660c12c6dd9efb1cbf0e26138524e7aea7729
jinunzzan <lovebox6607@gmail.com>
2022-11-27 21:19:58 +0900

    커뮤니티 글쓰기 뷰

--- MaginotLine/ViewControllers/CommunityPostWriteViewController.swift
+++ MaginotLine/ViewControllers/CommunityPostWriteViewController.swift
@@ -7,23 +7,35 @@
 
 import UIKit
 
-class CommunityPostWriteViewController: UIViewController {
-
+class CommunityPostWriteViewController: UIViewController, UITextViewDelegate {
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var placeHolderLabel: UILabel!
+    
+    let textViewPlaceHolder = "지각러님의 이야기를 들려주세요."
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        textView.delegate = self
+        textView.textContainer.maximumNumberOfLines = 0
+        textView.textContainer.lineFragmentPadding = 0
+        
     }
     
-
+    func textViewDidChange(_ textView:UITextView){
+            if textView.text == "" {
+                placeHolderLabel.isHidden = false
+            } else {
+                placeHolderLabel.isHidden = true
+            }
+        }
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
 }
